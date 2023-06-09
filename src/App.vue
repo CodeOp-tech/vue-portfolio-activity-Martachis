94b582e957b2fb671acda50b8c126ab80bd9f8db
Martachis <m.albimuro@gmail.com>
2023-06-09 11:56:37 +0200

    Template y estilo de formulario aún no funciona

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <button @click="isAdmin = true" :class="{ 'color-admin-button': isAdmin === true}">ADMIN</button>
-    <button @click="isAdmin = false" :class="{ 'color-user-button': isAdmin === false}">USER</button>
+  <div id="principal-content">
+    <h1>My Portfolio</h1>
+    <div class="button-container">
+      <button @click="isAdmin = true" :class="{ 'color-admin-button': isAdmin === true}">ADMIN</button>
+      <button @click="isAdmin = false" :class="{ 'color-user-button': isAdmin === false}">USER</button>
+    </div>
     <admin-view v-if="isAdmin" @createProject="addProject" />
     <user-view v-else />
 
@@ -35,5 +37,53 @@ export default {
 </script>
 
 <style scoped>
-  @import './styles.css';
+  .color-user-button, 
+.color-admin-button{
+    background-color: #4CAF50;
+    border-radius: 5px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    
+  }
+  
+  .color-admin-button {
+    border: none;
+  }
+
+  button {
+    margin: 10px;
+    margin-bottom: 30px;
+    border: none;
+    background-color: #BFC9BC;
+    padding: 2px 32px;
+    border-radius: 5px;
+    font-family: 'Lucida Sans';
+  }
+  
+  h1 {
+    font-family: 'Lucida Sans';
+    color: #4CAF50;
+  }
+
+  #principal-content{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    font-family: 'Lucida Sans';
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 40px; 
+  }
+  
+  .button-container button {
+    margin: 0 10px; 
+  }
 </style>
--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    Title
-    <input v-model="title" />
-    <button @click="handleSubmit">SUBMIT</button>
+    <h2>CREATE NEW PROJECT</h2>
+    <form>
+        <div class="title-url-fields">
+
+          <div class="text-field">
+            <label htmlForm="project-title">Project title</label>
+            <input id="project-title" type="text" name="project-title" v-model="title" />
+          </div>
+
+          <div class="text-field">
+            <label htmlForm="project-url">Image URL</label>
+            <input id="project-url" type="url" name="project-url" v-model="image" />
+          </div>
+
+        </div>
+        <div class="text-field">
+            <label htmlForm="project-decription">Project Description</label>
+            <input id="project-decription" type="text" name="project-decription" v-model="description" />
+        </div>
+        <div class="button-cointainer">
+          <button type="submit" @click="handleSubmit">SUBMIT</button>
+        </div>
+    </form>
   </div>
 </template>
 
 <script>
+
+
+
 export default {
   name: "AdminView",
   data() {
@@ -28,7 +51,15 @@ export default {
 };
 </script>
 
-<style>
+
+
+<style scoped>
+
+.button-cointainer{
+  justify-content: center;
+  display: flex;
+}
+
 button {
   background-color: #F9BB62;
   border-radius: 5px;
@@ -39,6 +70,49 @@ button {
   display: inline-block;
   font-size: 16px;
   border: none;
-  margin-left: 15px;
+  display: flex;
+  margin-top: 30px;
+  cursor: pointer;
 }
+
+button:hover{
+  background-color: #eed8bc;
+}
+
+button:active{
+  background-color: #9f5f05;
+}
+
+h2{
+  font-family: monospace;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #F9BB62;
+  margin-bottom: 30px;
+}
+
+.text-field{
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  font-family: 'Lucida Sans';
+}
+.title-url-fields {
+  display: flex;
+  margin-bottom: 40px;
+  gap: 20px;
+}
+
+#project-decription{
+  height: 80px;
+}
+
+input {
+  border: none;
+  background-color: rgb(250, 228, 196);
+  border-radius: 8px;
+  padding-top: 10px;
+}
+
 </style>
